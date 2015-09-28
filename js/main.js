587c7cb38dc636dc3c3b5416de946c1394d73479
Graphic <graphic@SOD-SJH-515-03M.local>
2015-09-28 10:40:00 -0400

    marina

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.signin').on('click', function (e){
+	$('.modal').fadeIn('slow');
+
+
+		$('.close').on('click', function (e){
+			$('.modal').fadeOut(300);
+		});
+
+
+//$('.submit').on('click' function (e){
+//$('.input').addClass('error');
+})
+		
+
+});
